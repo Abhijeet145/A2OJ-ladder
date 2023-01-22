62024fc549f877931b5434e0c68fa055f881b56e
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-01-23 00:37:55 +0530

    Add 186A and 327B

---
+++ 1300/327B.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++)
+    cout << (2*n + i)<<" ";
+    cout << "\n";
+    return 0;
+}

78093e192135188041ffb4dcf12e6fcba3a5b20b
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-02-18 16:50:34 +0530

    add template for 1300-1399

---
+++ 1300__1399/TemplateC++.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Type Names
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+
+//Macros (they replace the words with exact code so be careful while using them)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define FOR(i,a,b) for(ll i = a;i <= b; i++)
+
+
+
+
+
+////////////////////////Template ends////////////////////////
+void solve(ll tc)
+{
+    //start coding here
+    
+    cout <<"case: "<<tc<<endl;
+}
+
+////////////////// main function to call test cases//////////////
+int main()
+{
+    ll t,tc=1;
+    cin >> t;
+    while(t--)
+    {
+        solve(tc++);
+    }
+    return 0;
+}

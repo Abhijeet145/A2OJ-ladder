672fa0668fe67c9a97b50760592c27f6bb971031
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-02-22 08:04:44 +0530

    Add 276A

--- 1300__1399/276A.cpp
+++ 1300__1399/276A.cpp
@@ -13,17 +13,8 @@ typedef pair<int,int> pi;
 #define MP make_pair
 #define FOR(i,a,b) for(ll i = a;i < b; i++)
 
-
-
-
-
 ////////////////////////Template ends////////////////////////
-void solve(ll tc)
-{
-    //start coding here
-    
-    cout <<"case: "<<tc<<endl;
-}
+
 
 ////////////////// main function to call test cases//////////////
 int main()
@@ -36,11 +27,17 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    ll t,tc=1;
-    cin >> t;
-    while(t--)
-    {
-        solve(tc++);
+    ll n,k;
+    cin >> n >> k;
+    ll ans = -1e12;
+    ll f,t;
+    FOR(i,0,n){
+        cin >> f >>t;
+        if(t<=k)
+            ans = max(ans,f);
+        else
+            ans = max(ans,f-(t-k));
     }
+    cout << ans <<"\n";
     return 0;
 }

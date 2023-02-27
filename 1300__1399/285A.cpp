8fdd11324fd5b4fb5896a9f0a2a6dff1cbaee948
Abhijeet Kumar <abhijeet.19403@knit.ac.in>
2023-02-27 19:31:22 +0530

    Add 285A

---
+++ 1300__1399/285A.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Type Names
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+
+//Macros (they replace the words with exact code so be careful while using them)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define FOR(i,a,b) for(ll i = a;i < b; i++)
+#define f0tn for(int i=0;i<n;i++)
+
+
+
+
+////////////////////////Template ends////////////////////////
+ 
+
+////////////////// main function to call test cases//////////////
+int main()
+{
+    //to raad and write files from text files
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    //for faster input and output operations
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n,k;
+    cin >> n >> k;
+    for(int i=1;i<(n-k);i++)
+    cout << i <<" ";
+    for(int i=n;i>(n-k-1);i--){
+        cout << i <<" ";
+    }
+    cout <<"\n";
+    return 0;
+}
